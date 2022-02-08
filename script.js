1b2828ce7169e4c440b8619f377364ac05c0d1e2
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-02-08 18:14:06 +0000

    category for non-alcoholic, alcoholic beverages

--- script.js
+++ script.js
@@ -1,136 +1,136 @@
-var searchHistory = []
+var searchHistory = [];
 
 function randomCocktail() {
-    return fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
     .then((response) => response.json())
     .then((details) => {
-        randomCocktailHTML(details)
+      randomCocktailHTML(details);
     })
-    .catch((error) => console.log(error))
+    .catch((error) => console.log(error));
 }
 
-var searched = document.getElementById('userInput')
-var cocktailFormEl = document.getElementById('cocktailForm')
+var searched = document.getElementById("userInput");
+var cocktailFormEl = document.getElementById("cocktailForm");
 
 cocktailFormEl.addEventListener("submit", (event) => {
-    event.preventDefault()
+  event.preventDefault();
 
-    var searchValue = searched.value
-   
-    if (!searchValue) {
-        errorMsg("We need a name for a drink plsssssss thank you :)")
-    } else {
-        userInputCocktail(searchValue)
-        console.log(searchValue)
-    }
-})
+  var searchValue = searched.value;
 
+  if (!searchValue) {
+    errorMsg("We need a name for a drink plsssssss thank you :)");
+  } else {
+    userInputCocktail(searchValue);
+    console.log(searchValue);
+  }
+});
 
-function userInputCocktail (somethingIdk) {
-    return fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${somethingIdk}`)
+function userInputCocktail(somethingIdk) {
+  return fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${somethingIdk}`
+  )
     .then((response) => response.json())
     .then((details) => {
-        var { drinks }= details
-        if (drinks === null) {
-            errorMsg("It seems the drink does not exist. Big Ooofs.") 
-        } else {
-            userCocktailHTML(details)
-            if (!searchHistory.includes(somethingIdk)) {
-                searchHistory.push(somethingIdk)
-                localStorage.setItem("searchHistory" , JSON.stringify(searchHistory))
-                loadSearchHistory()
-            }
+      var { drinks } = details;
+      if (drinks === null) {
+        errorMsg("It seems the drink does not exist. Big Ooofs.");
+      } else {
+        userCocktailHTML(details);
+        if (!searchHistory.includes(somethingIdk)) {
+          searchHistory.push(somethingIdk);
+          localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+          loadSearchHistory();
         }
-    })
+      }
+    });
 }
 
-function storedDrinkSearch (somethingIdk) {
-    return fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${somethingIdk}`)
+function storedDrinkSearch(somethingIdk) {
+  return fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${somethingIdk}`
+  )
     .then((response) => response.json())
     .then((details) => {
-            userCocktailHTML(details)
-    })
+      userCocktailHTML(details);
+    });
 }
-
 
 function randomCocktailHTML(details) {
-    var { drinks } = details
-    var picture = document.getElementById('picture')
-    picture.innerHTML = `<img class="drinkThumb" src= "${drinks[0].strDrinkThumb}">`
+  var { drinks } = details;
+  var picture = document.getElementById("picture");
+  picture.innerHTML = `<img class="drinkThumb" src= "${drinks[0].strDrinkThumb}">`;
 
-    var cocktailName = document.getElementById('name')
-    cocktailName.innerHTML = `<h4> ${drinks[0].strDrink} </h4>
-    <p>Type of glass:</p><p>${drinks[0].strGlass}</p>`
+  var cocktailName = document.getElementById("name");
+  cocktailName.innerHTML = `<h4> ${drinks[0].strDrink} </h4>
+    <p>Type of glass:</p><p>${drinks[0].strGlass}</p>`;
 
+  var cocktailInformation = document.getElementById("ingridients");
+  var information = "";
+  var ingredientInfo = [];
+  for (var i = 1; i <= 15; i++) {
+    var measuringKey = "strMeasure" + i;
+    var ingridientKey = "strIngredient" + i;
+    var ingredient = drinks[0][ingridientKey];
 
-    var cocktailInformation = document.getElementById('ingridients')
-    var information = ''
-    var ingredientInfo = []
-    for (var i = 1; i <=15; i++) {
-        var measuringKey = 'strMeasure' + i
-        var ingridientKey = 'strIngredient' + i
-        var ingredient = drinks[0][ingridientKey]
+    if (!ingredient) break;
 
-        if (!ingredient) break;
-
-        information = `<p>${drinks[0][measuringKey]} ${ingredient}</p>`
-        ingredientInfo.push(information)
-    }
-    cocktailInformation.innerHTML = ingredientInfo.join('')
+    information = `<p>${drinks[0][measuringKey]} ${ingredient}</p>`;
+    ingredientInfo.push(information);
+  }
+  cocktailInformation.innerHTML = ingredientInfo.join("");
 }
 
 function userCocktailHTML(details) {
-    var { drinks } = details
-    var picture = document.getElementById('picture')
-    picture.innerHTML = `<img class="drinkThumb" src= "${drinks[0].strDrinkThumb}">`
-    
-    var cocktailName = document.getElementById('name')
-    cocktailName.innerHTML = `<h4> ${drinks[0].strDrink} </h4>
-    <p>Type of glass:</p><p>${drinks[0].strGlass}</p>`
+  var { drinks } = details;
+  var picture = document.getElementById("picture");
+  picture.innerHTML = `<img class="drinkThumb" src= "${drinks[0].strDrinkThumb}">`;
 
-    var cocktailInformation = document.getElementById('ingridients')
-    var information = ''
-    var ingredientInfo = []
-    for (var i = 1; i <=15; i++) {
-        var measuringKey = 'strMeasure' + i
-        var ingridientKey = 'strIngredient' + i
-        var ingredient = drinks[0][ingridientKey]
+  var cocktailName = document.getElementById("name");
+  cocktailName.innerHTML = `<h4> ${drinks[0].strDrink} </h4>
+    <p>Type of glass:</p><p>${drinks[0].strGlass}</p>`;
 
-        if (!ingredient) break;
+  var cocktailInformation = document.getElementById("ingridients");
+  var information = "";
+  var ingredientInfo = [];
+  for (var i = 1; i <= 15; i++) {
+    var measuringKey = "strMeasure" + i;
+    var ingridientKey = "strIngredient" + i;
+    var ingredient = drinks[0][ingridientKey];
 
-        information = `<p>${drinks[0][measuringKey]} ${ingredient}</p>`
-        ingredientInfo.push(information)
-    }
-    cocktailInformation.innerHTML = ingredientInfo.join('')
+    if (!ingredient) break;
+
+    information = `<p>${drinks[0][measuringKey]} ${ingredient}</p>`;
+    ingredientInfo.push(information);
+  }
+  cocktailInformation.innerHTML = ingredientInfo.join("");
 }
 
 function loadSearchHistory() {
-    searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
-    
-    var container = $("#searchHistory");
-    container.empty();
-    if (searchHistory !== null) {
-      for (var i = 0; i < searchHistory.length; i++) {
-        var pEl = $("<p>");
-        pEl.attr("class", "control");
-        var btnEl = $("<button>");
-        btnEl.attr("class", "button is-link history");
-        btnEl.html(searchHistory[i]);
-        pEl.append(btnEl);
-        container.append(pEl);
-      
+  searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+
+  var container = $("#searchHistory");
+  container.empty();
+  if (searchHistory !== null) {
+    for (var i = 0; i < searchHistory.length; i++) {
+      var pEl = $("<p>");
+      pEl.attr("class", "control");
+      var btnEl = $("<button>");
+      btnEl.attr("class", "button is-link history");
+      btnEl.html(searchHistory[i]);
+      pEl.append(btnEl);
+      container.append(pEl);
     }
-    }
+  }
 }
 
 function invokePastSearch(event) {
-    var btnEl = event.target;
-    if (event.target.matches(".history")) {
-      var somethingIdk = btnEl.textContent.trim();
-      storedDrinkSearch(somethingIdk);
-    }
+  var btnEl = event.target;
+  if (event.target.matches(".history")) {
+    var somethingIdk = btnEl.textContent.trim();
+    storedDrinkSearch(somethingIdk);
+  }
 }
-  
+
 function errorMsg(msg) {
   $("#modalId").addClass("is-active");
   $("#modalMsg").html(msg);
@@ -140,3 +140,52 @@ function close() {
 }
 $("#modalBtn").click(close);
 $(document).on("click", invokePastSearch);
+
+function byDrink() {
+  fetch(
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      getnameInfo(data);
+      console.log(data);
+    });
+}
+
+function getnameInfo(data) {
+  data.drinks.forEach((drinks) => {
+    var allCategoryNode = document.querySelector("#allCategories");
+
+    var categoryButton = document.createElement("button");
+    categoryButton.innerHTML = drinks.strDrink;
+
+    allCategoryNode.appendChild(categoryButton);
+  });
+}
+
+function alcholicDrink() {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      getDrinkInfo(data);
+    });
+
+  function getDrinkInfo(data) {
+    data.drinks.forEach((drinks) => {
+      var allCategoryNode = document.querySelector("#allCategories");
+
+      var categoryButton = document.createElement("button");
+      categoryButton.innerHTML = drinks.strDrink;
+
+      allCategoryNode.appendChild(categoryButton);
+    });
+  }
+}
+
+$(".Non-Alcoholic").on("click", byDrink);
+$(".Alcoholic").on("click", alcholicDrink);
